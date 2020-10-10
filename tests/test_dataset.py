9c6a1b79713fab5cc20c0908850e2302f6d0e7be
SijinKim <sijinkim@YBMacbook.local>
2020-10-10 19:26:07 +0900

    Create dataset for monet paintings (#1)

---
+++ tests/test_dataset.py
@@ -0,0 +1,15 @@
+import unittest
+from kaggle_0.dataset import MonetDataset
+
+
+class TestMonetDataset(unittest.TestCase):
+    def setUp(self):
+        self.dataset = MonetDataset()
+
+    def test_shape_of_loaded_image_array(self):
+        data = self.dataset[0]
+        self.assertEqual(data.shape, (256, 256, 3))
+
+    def test_number_of_images(self):
+        n_data = len(self.dataset)
+        self.assertEqual(n_data, 300)
